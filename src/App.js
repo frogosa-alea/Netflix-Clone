eb123cf5a109ccb62ae04e730740188de7c7dee0
frogosa-alea <benitezalea.f@gmail.com>
2022-08-20 23:12:32 +0800

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-white">
-      Hello world!
-    </h1>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
