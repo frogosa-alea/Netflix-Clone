e9e7d045ccd66789c15d30bd9b86e06d21536001
frogosa-alea <benitezalea.f@gmail.com>
2022-08-24 00:26:09 +0800

    Main and Nav Redesign

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import requests from "../Request";
-
+import { MdGrade, MdPlayArrow, MdQueuePlayNext } from "react-icons/md";
 const Main = () => {
   const [movies, setMovies] = useState([]);
 
   const movie = movies[Math.floor(Math.random() * movies.length)];
+  console.log("movie", movie);
 
   useEffect(() => {
-    axios.get(requests.requestPopular).then((response) => {
+    axios.get(requests.requestTrending).then((response) => {
       setMovies(response.data.results);
     });
   }, []);
@@ -21,22 +22,36 @@ const Main = () => {
 
   return (
     <div className="w-full h-[550px] text-white">
-      <div className="q-full h-full">
+      <div className="w-full h-full">
         <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div className="absolute w-full top-[20%] p-4 md:p-8">
+        <div className="absolute w-full top-[20%] px-12 py-4 md:mx-12 md:my-4 ">
+          <div className="grid grid-flow-col auto-cols-max gap-5 my-2 items-center text-sm">
+            <div className="grid grid-flow-col auto-cols-max items-center gap-2">
+              <MdGrade className="text-yellow-300" />
+              {movie?.vote_average}
+            </div>
+            <div>
+              <p className="uppercase">{movie?.media_type}</p>
+            </div>
+          </div>
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
-          <div className="my-4">
-            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+          <div className="my-4 flex gap-4">
+            <button className="text-black bg-yellow-300 flex items-center uppercase rounded-full px-4 py-2 text-center shadow-md shadow-slate-500/50">
+              <MdPlayArrow className="mr-2 h-5 w-5 " aria-hidden="true" />
               Play
             </button>
-            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+            <button className="text-black bg-sky-200 flex items-center uppercase rounded-full px-4 py-2 text-center shadow-md shadow-slate-500/50">
+              <MdQueuePlayNext className="mr-2 h-5 w-5 " aria-hidden="true" />
               Watch Later
             </button>
+            {/* <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button> */}
           </div>
           <p className="text-gray-400 text-sm">
             Released: {movie?.release_date}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
-
+import { MdAccountCircle } from "react-icons/md";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
 const Navbar = () => {
   const { user, logOut } = UserAuth();
   const navigate = useNavigate();
@@ -17,29 +19,77 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
-        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
-          NETFLIX
+        <h1
+          className=" text-3xl text-yellow-400 font-bold cursor-pointer"
+          style={{ fontFamily: "Beon" }}
+        >
+          Lights Out!
         </h1>
       </Link>
       {user?.email ? (
-        <div>
-          <Link to="/account">
-            <button className="text-white pr-4">Account</button>
-          </Link>
-          <button
-            onClick={handleLogout}
-            className="bg-red-600 px-6 py-2 rounded cursor-pointer"
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center rounded-full bg-black bg-opacity-20 px-1 py-1 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+              <MdAccountCircle className="text-3xl" />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            Logout
-          </button>
-        </div>
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  <button className="w-full rounded-md px-2 py-2 text-sm">
+                    Signed in as <p class="truncate ...">{user?.email}</p>
+                  </button>
+                </Menu.Item>
+              </div>
+              <div className="px-1 py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      to="/account"
+                      className={`${
+                        active
+                          ? "bg-sky-200 text-black font-bold"
+                          : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      My Account
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={handleLogout}
+                      className={`${
+                        active
+                          ? "bg-sky-200 text-black font-bold"
+                          : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Logout
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       ) : (
         <div>
           <Link to="/login">
             <button className="text-white pr-4">Sign In</button>
           </Link>
           <Link to="/signup">
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer">
+            <button className="bg-yellow-400 px-6 py-2 rounded-full cursor-pointer">
               Sign Up
             </button>
           </Link>
